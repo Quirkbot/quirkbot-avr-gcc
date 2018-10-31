44063bd667d39e52d363cde5d441416fa76e17c4
Paulo Barcelos <paulo@azucrina.org>
2018-10-31 14:43:15 +0100

    Removed win32-64

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var platforms = [
 	'linux-ia32',
 	'linux-x64',
 	'win32-ia32',
-	'win32-x64'
+	// 'win32-x64'
 ]
 
 // Move the current platform, if there is no tools directory
@@ -30,8 +30,9 @@ try {
 	console.log('./tools directory already exists.')
 } catch (e) {
 	console.log(`Moving ./tools directory.`)
+	const name = `${process.platform}-${process.platform === 'win32' ? 'ia32' : process.arch}`
 	fs.renameSync(
-		path.resolve(process.platform + '-' + process.arch, 'tools'),
+		path.resolve(name, 'tools'),
 		path.resolve('tools')
 	)
 }
